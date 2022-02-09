43c11a0612757804ecc2bf71149ef0b352e96b81
PengShuai <1447902621@qq.com>
2022-02-09 13:25:56 +0800

    https://github.com/PengShuai1995/C_PLUS_PLUS.git

---
+++ backtracking/greedy_algorithms/dijkstra.cpp
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <iostream>
+
+using namespace std;
+
+//Wrapper class for storing a graph
+class Graph{
+    public:
+    int vertexNum;
+    int **edges;
+
+    //Constructs a graph with V vertices and E edges
+    Graph(const int v)
+    {
+        
+    }
+}
